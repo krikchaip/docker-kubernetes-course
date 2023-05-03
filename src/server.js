791ddf383d0319dd8bed5a288eee00fbed84e720
krikchaip <krikchai.p@gmail.com>
2023-05-03 13:23:51 +0700

    redis foundation setup

--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
 import express from 'express'
+import { createClient } from 'redis'
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 8080
+const REDIS_URL = process.env.REDIS_URL ?? 'redis://localhost:6379'
 
 const app = express()
+const redisClient = createClient({ url: REDIS_URL })
 
 app.get('/', (req, res) => {
   res.send('OK')
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  redisClient.on('error', console.error)
+  await redisClient.connect()
+
+  console.log(`connected to redis-server at ${REDIS_URL}`)
   console.log(`server is listening at port ${PORT}`)
 })
