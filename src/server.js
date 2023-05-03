5dafc5c597eea78d7cffa11e61761a789c039921
krikchaip <krikchai.p@gmail.com>
2023-05-04 02:11:15 +0700

    server: integrate redis to the code

--- src/server.js
+++ src/server.js
@@ -7,13 +7,19 @@ const REDIS_URL = process.env.REDIS_URL ?? 'redis://localhost:6379'
 const app = express()
 const redisClient = createClient({ url: REDIS_URL })
 
-app.get('/', (req, res) => {
-  res.send('OK')
+app.get('/', async (req, res) => {
+  const visits = await redisClient.incr('visits')
+  res.send(`<h1>Number of visits: ${visits}</h1>`)
 })
 
 app.listen(PORT, async () => {
   redisClient.on('error', console.error)
   await redisClient.connect()
+
+  if (!redisClient.isReady) {
+    console.error(`redis connection failed!`)
+    return process.exit(1)
+  }
 
   console.log(`connected to redis-server at ${REDIS_URL}`)
   console.log(`server is listening at port ${PORT}`)
